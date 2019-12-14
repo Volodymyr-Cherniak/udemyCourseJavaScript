9e65c5b23ff08dcab454fb427a37b12a9e9fd8db
Volodymyr-Cherniak <vovastalker26@gmail.com>
2019-12-14 20:14:06 +0100

    test proj

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let person = {
+    name: "John",
+    age: 30,
+    isMaried: true
+};
+
+console.log(person['age']);
+
+//alert('Hello world');
+let ansver = prompt('Have you 18 years?')
+
+console.log(ansver);
